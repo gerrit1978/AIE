e625d86028d5ec3c503a9cff037dfdce016d7f98
Gerrit Vos <gevo27@gmail.com>
2015-06-03 15:21:18 +0200

    ...

--- javascripts/aie.js
+++ javascripts/aie.js
@@ -2,4 +2,12 @@ jQuery(document).ready(function($) {
   $('.flexslider').flexslider({
     slideshowSpeed: 3000
   });
+  
+  // subitems on menu
+  $('.menu ul li').click(function(e) {
+    if ($(this).children("ul.subitems").length) {
+      $(this).toggleClass("expanded");
+      $(this).children("ul.subitems").toggle();
+    } 
+  });
 });
