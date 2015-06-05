1be769f245e206d4a25d00c089b8ba1afc4e385a
Gerrit Vos <gevo27@gmail.com>
2015-06-05 07:58:47 +0200

    ...

--- javascripts/aie.js
+++ javascripts/aie.js
@@ -1,12 +1,43 @@
 jQuery(document).ready(function($) {
+
+  // slider for carousel
   $('.flexslider').flexslider({
     slideshowSpeed: 3000,
     directionNav: false
   });
   
+  // slider for partners
+  var width = $(window).width();
+  if (width < 400) {
+    var newWidth = width * 0.95;
+  } else {
+    if (width > 1024) {
+      var newWidth = 160;
+    }
+  }
   $('.flexslider-partners').flexslider({
     animation: "slide",
-    controlNav: false
+    controlNav: false,
+    itemWidth: newWidth,
+    minItems: 1,
+    maxItems: 6,
+    move: 1
+  });
+  
+  // responsive menu
+  $('section.menu .trigger').click(function(e) {
+    var menu = $(this).parent().find('ul.items');
+    var left = menu.css('left').replace('px', '');
+    if (left > 300) {
+      menu.animate({
+        left: "5%"
+      }, 800, 'easeInOutQuart');
+    } else {
+      // rudimentaire close functie
+      menu.animate({
+        left: "100%"
+      }, 500, 'easeOutCubic');
+    }
   });
   
   // subitems on menu
